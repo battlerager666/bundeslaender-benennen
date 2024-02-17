b5baf18f34633119d05077ed5346946755b95f5f
battlerager666 <155563645+battlerager666@users.noreply.github.com>
2024-02-17 21:11:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,120 +1,71 @@
-var alleBuchstaben = Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-var wievielteRunde = 0;
-var ShowTimerMinutes;
-var timerSecondsTotal;
-var ShowTimerSeconds;
-let timerAktiv;
+const alleBundesländer = Array("Schleswig-Holstein","Mecklenburg-Vorpommern","Hamburg","Niedersachsen","Bremen","Thüringen","Sachsen","Sachsen-Anhalt","Brandenburg",
+"Berlin","Nordrhein-Westfalen","Hessen","Rheinland-Pfalz","Saarland","Baden-Württemberg","Bayern");
 
-function changeBackgroundColor(color){
+let zufälligesBundesland;
+
+function neueRunde() { //neues bundesland anzeigen
+    zufälligesBundesland = alleBundesländer[Math.floor(Math.random()*alleBundesländer.length)];
+    
+    document.getElementById("frage").innerText = zufälligesBundesland;
+
+    if(hilfenAngezeigt === true){
+        buttonColorÄndernEinzeln("red");
+    }
+}
+
+function AnswerGiven(Answer) {
+    if(Answer === zufälligesBundesland) { //wenn richtig: neue runde und grüner hintergrund
+        console.log("richtig");
+        changeBackground("rgb(101, 175, 101)"); //grün
+        setTimeout(() => {
+            changeBackground("rgb(221, 173, 110)"); //normalfarbe
+          }, 500);
+
+        neueRunde();
+    } else{ //wenn falsch: roter hintergrund
+        console.log("falsch");
+        changeBackground("rgb(218, 78, 78)"); //rot
+        setTimeout(() => {
+            changeBackground("rgb(221, 173, 110)"); //normalfarbe
+          }, 500);
+    }
+}
+
+function changeBackground(color){
     document.body.style.background = color;
 }
 
-function neuesSpielStarten(){
-    alleBuchstaben = new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"); //zurücksetzen
-
-    var übrigeBuchstabenZahlBestimmen = document.getElementById("p_Anzeige_übrigeBuchstaben"); //wieviele übrig? zurücksetzen
-    übrigeBuchstabenZahlBestimmen.innerText = "Übrige Buchstaben: " + alleBuchstaben.length;
-
-    var ergebnisAnzeige = document.getElementById("p_Anzeige_welcherBuchstabe"); //buchstabenAnzeige zurücksetzen
-    ergebnisAnzeige.innerText = "...";
-
-    wievielteRunde = 0;
-    var wievielteRundeAnzeige = document.getElementById("p_Anzeige_wievielteRunde"); //runde updaten zurücksetzen
-    wievielteRundeAnzeige.innerText = "Runde: 0";
-
+function buttonColorÄndernEinzeln(color){ //bei hilfe an: hintergrund der buttons rot
+    document.getElementById(zufälligesBundesland).style.background = color;
 }
 
-function zufälligerBuchstabeAuslösen(){
-    if(alleBuchstaben.length > 0) {
-    let zufälligerBuchstabe = alleBuchstaben[Math.floor(Math.random()*alleBuchstaben.length)]; //random Buchstabe
+const bundesländerHilfen = [];
+var hilfenAngezeigt = false;
 
-    welcherIndex = alleBuchstaben.indexOf(zufälligerBuchstabe); //verwendeten Buchstaben entfernen
-    alleBuchstaben.splice(welcherIndex, 1);
-
-    var ergebnisAnzeige = document.getElementById("p_Anzeige_welcherBuchstabe"); //den buchstaben anzeigen
-    ergebnisAnzeige.innerText = zufälligerBuchstabe;
-
-    var übrigeBuchstabenZahlBestimmen = document.getElementById("p_Anzeige_übrigeBuchstaben"); //wieviele übrig? anzeigen
-    übrigeBuchstabenZahlBestimmen.innerText = "Übrige Buchstaben: " + alleBuchstaben.length;
-
-    wievielteRunde++;
-    var wievielteRundeAnzeige = document.getElementById("p_Anzeige_wievielteRunde"); //runde updaten + anzeigen
-    wievielteRundeAnzeige.innerText = "Runde: " + wievielteRunde;
-    }
-}
-
-function StoppuhrAuslösen(){
-    changeBackgroundColor("white");
-
-    if(document.getElementById("input_Stoppuhr_Zeit").value > 0){
-    ShowTimerMinutes = document.getElementById("input_Stoppuhr_Zeit").value; //timer Minuten insgesamt
-    } else{
-    ShowTimerMinutes = 2;
-    }
-
-    timerSecondsTotal = ShowTimerMinutes * 60; //timer Sekunden insgesamt
-
-    timerSecondsTotal++ //für die erste sekunde anzeigen
-    StoppuhrUpdaten();
-
-    if(timerAktiv === undefined){
-    timerAktiv = setInterval(StoppuhrUpdaten, 1000); //timer aktivieren (kann nur einmal ausgeführt werden)
-    }
-}
-
-function StoppuhrUpdaten(){
-    timerSecondsTotal--;
-    ShowTimerMinutes = Math.trunc(timerSecondsTotal / 60);
-    ShowTimerSeconds = timerSecondsTotal - (ShowTimerMinutes * 60); //showTimerMinutes und showTimerSeconds
-
-    if(ShowTimerMinutes <= 0 && ShowTimerSeconds <= 0){ //wenn beides = 0, reset
-        clearInterval(timerAktiv);
-        timerAktiv = undefined;
-        changeBackgroundColor("orange");
-    }
-
-    if(ShowTimerMinutes < 10){
-        ShowTimerMinutes = "0" + ShowTimerMinutes; //2:0 -> 02:00
-    }
-    if(ShowTimerSeconds < 10){
-        ShowTimerSeconds = "0" + ShowTimerSeconds;
-    }
-
-    document.getElementById("p_Anzeige_Stoppuhr").innerText = ShowTimerMinutes + " : " + ShowTimerSeconds; //zeit anzeigen
-
-    //console.log(ShowTimerMinutes, ShowTimerSeconds, timerSecondsTotal, timerAktiv); //testen
-}
-
-function StoppuhrBeenden(){ //alles resetten
-    clearInterval(timerAktiv);
-
-    timerAktiv = undefined;
-
-    changeBackgroundColor("white");
-
-    zeitAnzeigen();
-}
-
-function zeitAnzeigen(){
-    if(document.getElementById("input_Stoppuhr_Zeit").value > 0){
-        ShowTimerMinutes = document.getElementById("input_Stoppuhr_Zeit").value; //timer Minuten insgesamt
-        } else{
-        ShowTimerMinutes = 2;
+document.getElementById("btn_hilfeUmschalten").addEventListener("click", function(e){
+    if(hilfenAngezeigt === true){
+        for(let i = 0; i < alleBundesländer.length; i++){ //alle namen löschen
+            document.getElementById(alleBundesländer[i]).innerText = "";
         }
-    
-    timerSecondsTotal = ShowTimerMinutes * 60; //timer Sekunden insgesamt
-
-    ShowTimerMinutes = Math.trunc(timerSecondsTotal / 60);
-    ShowTimerSeconds = timerSecondsTotal - (ShowTimerMinutes * 60); //showTimerMinutes und showTimerSeconds
-
-    if(ShowTimerMinutes < 10){
-        ShowTimerMinutes = "0" + ShowTimerMinutes; //2:0 -> 02:00
+        for(let i = 0; i < alleBundesländer.length; i++){ //jede farbe resetten
+            document.getElementById(alleBundesländer[i]).style.background = "rgba(214, 74, 55, 0.4)";
+        }
+        hilfenAngezeigt = false;
+    } else{
+        for(let i = 0; i < alleBundesländer.length; i++){ //alle namen anzeigen
+            document.getElementById(alleBundesländer[i]).innerText = bundesländerHilfen[i];
+        }
+        buttonColorÄndernEinzeln("red"); //farbe für erstes bundesland anzeigen
+        hilfenAngezeigt = true;
     }
-    if(ShowTimerSeconds < 10){
-        ShowTimerSeconds = "0" + ShowTimerSeconds;
-    }
-    
-    document.getElementById("p_Anzeige_Stoppuhr").innerText = ShowTimerMinutes + " : " + ShowTimerSeconds; //zeit anzeigen
+});
+
+neueRunde();
+
+for(let i = 0; i < alleBundesländer.length; i++){ //alle namen der bundesländer in einer array speichern
+    bundesländerHilfen.push(document.getElementById(alleBundesländer[i]).innerText);
 }
 
-zeitAnzeigen();
+for(let i = 0; i < alleBundesländer.length; i++){ //am anfang alle löschen
+    document.getElementById(alleBundesländer[i]).innerText = "";
+}
